ac345dd309dcd4dc35b086f7de63e2729951de62
isaac <oslinuc@gmail.com>
2025-06-10 04:20:02 +0300

    some modifications to the code and adit argparse library

--- ch2-basic-network/tcp_client.py
+++ ch2-basic-network/tcp_client.py
@@ -1,6 +1,16 @@
 import socket 
-target_host = "www.google.com"
-target_port = 80 
+import argparse
+
+parser = argparse.ArgumentParser(description="TCP client , Send Request & Receive  Response")
+parser.add_argument("-t" ,dest="target", help="target send to request")
+parser.add_argument("-p" ,dest="port" ,type=int, help="port")
+parser.add_argument("-m" ,dest="method" , help="any method : GET,HEAD,POST,PUT,DELETE,...")
+args = parser.parse_args()
+
+
+target_host = args.target
+target_port = args.port
+target_method = args.method
 
 #create a socket object 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -9,7 +19,8 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((target_host,target_port))
 
 #send some data 
-s.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+request = f"{target_method} / HTTP/1.1\r\nHost: {target_host}\r\n\r\n"
+s.send(request.encode())
 
 #receive some data 
 response = s.recv(4096)
--- ch2-basic-network/tcp_server.py
+++ ch2-basic-network/tcp_server.py
@@ -1,9 +1,13 @@
 import socket
 import threading
-import sys 
-
+import argparse
+import sys
+parser = argparse.ArgumentParser(description="tcp server")
+parser.add_argument("-p" , dest="port" , type=int , help="choose which port you want to build the server on from (1024-65,536)")
+args = parser.parse_args()
+# 127.0.0.1 this is local host in any client
 IP = "127.0.0.1"
-PORT = 8686
+PORT = args.port
 
 html_response = """/
 HTTP/1.1 200 OK 
@@ -12,7 +16,7 @@ Content-Type: text/html
 <html>
   <head><title>server</title></head>
   <body>
-      <h1>Hello from Python Server</h1>
+      <h1>Hello from the python server</h1>
   </body>
 </html>
 """
@@ -28,6 +32,8 @@ def main():
         print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
         cleint_handler = threading.Thread(target=handle_client, args=(cleint,))
         cleint_handler.start()
+        if KeyboardInterrupt:
+            sys.exit(0)
         
 def handle_client(client_socket):
     with client_socket as sock:
